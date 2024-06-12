0aad4cfe5da6327941d125f1ec6d4a32a2900e55
Davi do carmo <davigcarmo02@gmail.com>
2024-06-11 21:55:48 -0300

    terceiro-commit

--- src/main/java/br/senac/pr/api_pix_impresso/controllers/CaixaController.java
+++ src/main/java/br/senac/pr/api_pix_impresso/controllers/CaixaController.java
@@ -3,40 +3,59 @@ package br.senac.pr.api_pix_impresso.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.senac.pr.api_pix_impresso.dtos.CreateCaixaDto;
+import br.senac.pr.api_pix_impresso.dtos.UpdateCaixaDto;
 import br.senac.pr.api_pix_impresso.models.Caixa;
-import br.senac.pr.api_pix_impresso.repositories.JdbcCaixaRepository;
 import br.senac.pr.api_pix_impresso.services.CaixaService;
 
 @RestController
 @RequestMapping("/caixas")
 public class CaixaController {
-  private CaixaService caixaService;
-  
+    private CaixaService caixaService;
+
     public CaixaController(CaixaService caixaService) {
-    this.caixaService = caixaService;
-}
+        this.caixaService = caixaService;
+    }
 
     @GetMapping("")
-    public List<Caixa> gatCaixas() {
+    public List<Caixa> getCaixas() {
         return caixaService.findAll();
     }
 
     @PostMapping("")
     public Caixa createCaixa(@RequestBody CreateCaixaDto dto) {
         Caixa caixa = new Caixa(null,
-        dto.getLocalizacao(),
-        dto.getSaldo());
-        //salvar no banco
+                dto.getLocalizacao(),
+                dto.getSaldo());
+        // Salvar no banco
         int id = caixaService.save(caixa);
         caixa.setId(Long.valueOf(id));
+        // retornar o objeto caixa o id
         return caixa;
-        //service -> repository
-        //retornar objeto caixa id
     }
+
+    // GET por ID
+
+    // PATCH - Atualização parcial
+    @PatchMapping("")
+    public Caixa updateCaixa(@RequestBody UpdateCaixaDto dto,
+                             @RequestParam Long id) {
+    //criar obejto modelo
+        Caixa caixa = new Caixa(id,dto.getLocalizacao(),dto.getSaldo());
+        //atualizar o registro do banco
+        caixaService.update(caixa);
+        //retornar objeto caixa
+        return caixa;    
+    }
+
+    // PUT - Atualização completa
+    // DELETE - Exclusão
+
 }
--- src/main/java/br/senac/pr/api_pix_impresso/repositories/JdbcCaixaRepository.java
+++ src/main/java/br/senac/pr/api_pix_impresso/repositories/JdbcCaixaRepository.java
@@ -1,8 +1,13 @@
 package br.senac.pr.api_pix_impresso.repositories;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import br.senac.pr.api_pix_impresso.models.Caixa;
@@ -10,18 +15,40 @@ import br.senac.pr.api_pix_impresso.models.Caixa;
 @Repository
 public class JdbcCaixaRepository implements CaixaRepository {
 
-  
   private JdbcTemplate jdbcTemplate;
+  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-  public JdbcCaixaRepository(JdbcTemplate jdbcTemplate) {
+  public JdbcCaixaRepository(JdbcTemplate jdbcTemplate,
+      NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
+    this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
   }
 
   @Override
   public int save(Caixa caixa) {
-    return jdbcTemplate.update(
-        "INSERT INTO CAIXAS (LOCALIZACAO, SALDO) VALUES(?,?)",
-        new Object[] { caixa.getLocalizacao(), caixa.getSaldo() });
+    GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+
+    // SQL placeholders can use named parameters instead of "?".
+    String sql = """
+          INSERT INTO CAIXAS (LOCALIZACAO, SALDO)
+          VALUES(:localizacao, :saldo)
+        """;
+
+    Map<String, Object> params = new HashMap<>();
+    params.put("localizacao", caixa.getLocalizacao());
+    params.put("saldo", caixa.getSaldo());
+
+    namedParameterJdbcTemplate.update(sql,
+        new MapSqlParameterSource(params),
+        generatedKeyHolder);
+
+    var returnedKeys = generatedKeyHolder.getKeys();
+    if (returnedKeys == null) {
+      throw new Error("Erro ao salvar o caixa");
+    }
+    Integer id = (Integer) returnedKeys.get("ID");
+
+    return id;
   }
 
   @Override
@@ -43,19 +70,20 @@ public class JdbcCaixaRepository implements CaixaRepository {
   }
 
   @Override
-    public List<Caixa> findAll() {
-        return jdbcTemplate
-            .query("SELECT ID, LOCALIZACAO, SALDO FROM CAIXAS",
-                (rs, rowNum) -> {
-                    return new Caixa(rs.getLong("ID"),
-                        rs.getString("LOCALIZACAO"),
-                        rs.getDouble("SALDO"));
-        });
-      }
+  public List<Caixa> findAll() {
+    return jdbcTemplate
+        .query("SELECT ID, LOCALIZACAO, SALDO FROM CAIXAS",
+            (rs, rowNum) -> {
+              return new Caixa(rs.getLong("ID"),
+                  rs.getString("LOCALIZACAO"),
+                  rs.getDouble("SALDO"));
+            });
+  }
+
   @Override
   public int deleteAll() {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'deleteAll'");
   }
-    
-    }
+
+}
